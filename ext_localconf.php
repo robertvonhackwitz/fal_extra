572b29089e4c0a574df85c44a9189ff31d966469
Robert von Hackwitz <robertvonhackwitz@gmail.com>
2020-03-08 13:24:11 +0100

    2020-03-08 Added README

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,5 +8,3 @@ unset($rendererRegistry);
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['facebook'] = \RVH\FalExtra\Helpers\FacebookHelper::class;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['FileInfo']['fileExtensionToMimeType']['facebook'] = 'video/facebook';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'] .= ',facebook';
-
-
